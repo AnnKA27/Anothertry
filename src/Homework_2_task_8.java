acfbad01e8005af4aeccce427229c62b30756ae6
AnnKA27 <44210704+AnnKA27@users.noreply.github.com>
2018-10-17 17:00:10 +0300

    Создайте программу, выводящую на экран все неотрицательные элементы последовательности 90 85 80 75 70 65 60 ….

---
+++ src/Homework_2_task_8.java
@@ -0,0 +1,10 @@
+public class Homework_2_task_8 {
+
+    public static void main(String args[]) {
+        int i;
+        for (i = 90; i >= 0; i -= 5) {
+            System.out.println(i + " ");
+        }
+    }
+}
+
