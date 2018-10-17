63d2099a92ffd6d2a97e25b26f30d58c14d70614
AnnKA27 <44210704+AnnKA27@users.noreply.github.com>
2018-10-17 16:42:18 +0300

    Создайте программу, выводящую на экран все четырёхзначные числа последовательности 1000 1003 1006 1009 1012 1015 …

---
+++ src/Homework_2_task_6.java
@@ -0,0 +1,10 @@
+public class Homework_2_task_6 {
+
+    public static void main(String args[]) {
+        int i;
+        for (i = 1000; i<= 9999; i+=3) {
+            System.out.println(i + " ");
+        }
+    }
+
+}
