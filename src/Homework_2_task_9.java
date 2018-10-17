9686d7fdbab88e0f5ce3b00a570c546dcdc0b03d
AnnKA27 <44210704+AnnKA27@users.noreply.github.com>
2018-10-17 17:03:02 +0300

    Создайте программу, выводящую на экран первые 20 элементов последовательности 2 4 8 16 32 64 128 ….

---
+++ src/Homework_2_task_9.java
@@ -0,0 +1,10 @@
+public class Homework_2_task_9 {
+    public static void main(String args[]) {
+        int i;
+        int j = 1;
+        for (i = 2; j <= 20; i *= 2) {
+            j++;
+            System.out.println(i + " ");
+        }
+    }
+}
