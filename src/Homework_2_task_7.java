990ee9375c9debb8c2778ea072da76fc715fa10d
AnnKA27 <44210704+AnnKA27@users.noreply.github.com>
2018-10-17 16:57:00 +0300

    Создайте программу, выводящую на экран первые 55 элементов последовательности 1 3 5 7 9 11 13 15 17 ….

---
+++ src/Homework_2_task_7.java
@@ -0,0 +1,11 @@
+public class Homework_2_task_7 {
+    public static void main(String args[]) {
+        int i;
+        int j = 1;
+        for (i = 1; j <= 55; i += 2) {
+            j++;
+            System.out.println(i + " ");
+        }
+
+    }
+}
