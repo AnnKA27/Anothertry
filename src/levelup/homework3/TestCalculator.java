f796dca3186e22132d28be510e511ed5816b8a1c
AnnKA27 <44210704+AnnKA27@users.noreply.github.com>
2018-10-26 19:46:28 +0300

    Создать класс Calculator. У него должны быть методы: 1. Сумма 2. Деление 3. Умножение 4. Вычитание Все методы должны быть перегружены и работать с тремя типами - double, long и int.

---
+++ src/levelup/homework3/TestCalculator.java
@@ -0,0 +1,54 @@
+package levelup.homework3;
+
+public class TestCalculator {
+
+    public int summ(int a, int b) {
+        return a + b;
+    }
+
+    public double summ(double a, double b) {
+        return a + b;
+    }
+
+    public long summ(long a, long b) {
+        return a + b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public long divide(long a, long b) {
+        return a / b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public long multiply(long a, long b) {
+        return a * b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public long subtract(long a, long b) {
+        return a - b;
+    }
+
+}
+
